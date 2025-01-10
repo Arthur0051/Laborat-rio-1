0c453f6877a64a6214c9570d9222117639f1151d
Carlos Eduardo de Lima Pereira <al.carlos.pereira@impatech.org.br>
2025-01-10 18:13:49 -0300

    Update código_2.py

--- Prática 1/Códigos de cálculo/código_2.py
+++ Prática 1/Códigos de cálculo/código_2.py
@@ -1,22 +1,25 @@
-
 def calibracao_10(tensoes, r_2):
     a0, a1 = tensoes
     data = {'a0': a0, 'a1': a1}
 
     
     ddp_r_2 = data['a1']
-    corrente = ddp_r_2/r_2
+    corrente = ddp_r_2/r_2 
 
     ddp_r_1 =  data['a0'] - data['a1']
 
-    return ddp_r_1/corrente
+    return ddp_r_1/corrente , corrente
 
-tensao_A = [5.000, 4.823 ]
+
+tensao_A = [5.000, 4.765 ]
 tensao_B = [5.000, 4.765 ]
 tensao_C = [5.000, 4.765 ]
 
-r_1A =  calibracao_10(tensao_A, 200)
-r_1B =  calibracao_10(tensao_B, 200)
-r_1C =  calibracao_10(tensao_C, 200)
+r_1A, cA =  calibracao_10(tensao_A, 200)
+r_1B, cB =  calibracao_10(tensao_B, 200)
+r_1C, cC =  calibracao_10(tensao_C, 200)
+
 
 print(r_1A, r_1B, r_1C)
+print(r_1A/10, r_1B/10,r_1C/10)
+print(cA, cB, cC)
